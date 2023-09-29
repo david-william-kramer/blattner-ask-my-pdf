45324416e7bda118275432264437831e40678a2c
david-william-kramer <108325743+david-william-kramer@users.noreply.github.com>
2023-09-29 13:02:36 -0500

    Create homepage.py

---
+++ homepage.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+from PyPDF2 import PdfFileReader, PdfFileWriter,PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import pickle, os, base64
+from dotenv import load_dotenv
+import sys
+import os
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+st.title('📄 Ask My MePS: Windover Abu Dhabi')
+
+load_dotenv()
+
+with st.sidebar:
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    st.image("MePS app logo.png", use_column_width=True)
+
+
+if 'chat_history' not in globals():
+  chat_history = []
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = chat_history
+
+with st.form("chat_input", clear_on_submit=True):
+    a, b = st.columns([6, 1])
+    user_input = a.text_input(
+        label="Your message:",
+        placeholder="Ask a question about your document",
+        label_visibility="collapsed",
+    )
+    b.form_submit_button("Send", use_container_width=True)
+
+if "pdf" in globals() and pdf not in ["None", None]:
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text+= page.extract_text()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text=text)
+    
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(chunks,embedding=embeddings)
+    
+    qa_chain = ConversationalRetrievalChain.from_llm(ChatOpenAI(),
+                                                     vectorstore.as_retriever(search_kwargs={'k': 6}),
+                                                     return_source_documents=True)
+
+if user_input and "pdf" not in globals():
+    st.info("Please upload a document to continue")
+elif user_input:
+    try:
+        result = qa_chain({'question': user_input, 'chat_history': st.session_state.chat_history})
+        answer = result["answer"]
+        st.session_state.chat_history.append((user_input, answer))
+        st.info(f"You Asked: {user_input}\n\nThe Answer:\n\n{answer}")
+    except:
+        st.info("Please ask a question about your document to continue")
